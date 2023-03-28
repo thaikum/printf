fc819442eab297002b5a877960df2c3c1e22914d
bion-slmn <bionsol25@gmail.com>
2023-03-28 02:32:28 -0700

    binary

--- int_binary.c
+++ int_binary.c
@@ -10,7 +10,7 @@
  */
 int int_to_binary(va_list ptr)
 {
-	int i, len = 0;
+	int i, len = 0, count = 0;
 	char *binary;
 
 	unsigned int num = va_arg(ptr, unsigned int);
@@ -18,16 +18,25 @@ int int_to_binary(va_list ptr)
 	if (num == 0)
 		return (0);
 	len = base_len(num, 2);
-
-	binary = malloc(sizeof(char) * len + 1);
-
+	binary = malloc(sizeof(unsigned int) * len);
 	if (binary == NULL)
-		return (-1);
+	{
+		free(binary);
+		return (0);
+	}
+	if (num == 0)
+	{
+		_putchar(0 + '0');
+		free(binary);
+		return (1);
+	}
 	for (i = 0; num > 0; i++)
 	{
 		binary[i] = num % 2;
 		num = num / 2;
 	}
+	count = i;
+
 	binary[i] = '\0';
 
 	for (i = len - 1; i > 0; i--)
@@ -35,7 +44,5 @@ int int_to_binary(va_list ptr)
 		_putchar(binary[i] + '0');
 	}
 	free(binary);
-
-	return (len);
-
+	return (count);
 }
